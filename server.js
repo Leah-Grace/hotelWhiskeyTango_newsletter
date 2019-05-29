26d8160c6d4cf4a82ff9cc170bd35242a9dd9d15
Leah-Grace <leahgdg@hotmail.com>
2019-05-29 18:28:34 -0400

    included es lint

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const port = process.env.port || 4040;
 
 app.use(express.static('public'));
 
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 //serves Archived newsletters from Constant Contact
